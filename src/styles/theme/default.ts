c9e7d945b44780ade9c8c933b4a47594cbdca728
Tiago Vieira <tiagosvieira10@gmail.com>
2023-10-03 15:33:35 -0300

    commit inicial - config e defaultTheme prontos

---
+++ src/styles/theme/default.ts
@@ -0,0 +1,31 @@
+export const defaultTheme = {
+  colors: {
+    "blue": "#3294F8",
+    "base-title": "#E7EDF4",
+    "base-subtitle": "#C4D4E3",
+    "base-text": "#AFC2D4",
+
+    "base-span": "#7B96B2",
+    "base-label": "#3A536B",
+    "base-border": "#1C2F41",
+    "base-post": "#112131",
+
+    "base-profile": "#0B1B2B",
+    "base-background": "#071422",
+    "base-input": "#040F1A"
+  },
+
+  fontWeights: {
+    "title": "bold",
+    "text": "regular"
+  },
+
+  fontSizes: {
+    "title-l": "1.5rem",
+    "title-m": "1.25rem",
+    "title-s": "1.125rem",
+    "text-m": "1rem",
+    "text-s": "0.875rem",
+    "link": "0.75rem",
+  }
+}
